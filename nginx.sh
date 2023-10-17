b60c3f9a30d3e05ae7b325299c75aea41e28247d
Ivan <iluzjuk@gmail.com>
2023-10-17 21:12:32 +0300

    delete info

--- nginx.sh
+++
@@ -1,3 +0,0 @@
-helm repo add nginx-stable https://helm.nginx.com/stable
-helm repo update
-helm install nginx-kic nginx-stable/nginx-ingress --namespace nginx-ingress --set controller.enableCustomResources=true --create-namespace --set controller.enableCertManager=true
